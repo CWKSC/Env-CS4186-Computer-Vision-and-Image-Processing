607865d66dc36f52bc35eb560d999d9192caeade
CWKSC <cwksc.person@gmail.com>
2022-01-11 15:51:33 +0800

    Init ColorHistogram

---
+++ workspace/ColorHistogram/main.py
@@ -0,0 +1,11 @@
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread("image.jpg")
+
+colors = ("b", "g", "r")
+for i, color in enumerate(colors):
+    histr = cv2.calcHist([image], [i], None, [256], [0, 256])
+    plt.plot(histr, color=color)
+    plt.xlim([0, 256])
+plt.show()
