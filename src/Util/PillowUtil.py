6343f8c25044df5d66acf34f4c4efe5dce9f495c
CWKSC <53114952+CWKSC@users.noreply.github.com>
2023-08-15 00:40:54 +0800

    Update PillowUtil.py

--- src/Util/PillowUtil.py
+++ src/Util/PillowUtil.py
@@ -3,7 +3,6 @@ import builtins
 
 from PIL import Image
 from PIL.Image import Image as ImageType
-from py import builtin
 
 import Util.Array2DUtil as Array2DUtil
 import Util.KernelUtil as KernelUtil
@@ -20,7 +19,6 @@ def iterPixel(
     return ((pixels[x, y], x, y) for (x, y) in iter(image))
 
 
-
 def mapPixel(
     image: ImageType,
     mapPixel: Callable[
@@ -35,15 +33,18 @@ def mapPixel(
         pixels[r, c] = mapPixel(pixels[r, c], r, c)
     return image
 
+
 def getPixelArray2D(image: ImageType) -> list[list[Union[int, tuple[int, int, int]]]]:
     pixels = image.load()
     return [[pixels[x, y] for y in range(image.size[1])] for x in range(image.size[0])]
+
 
 def fromArray2D(array2d: list[list[Any]], mode: str) -> ImageType:
     (row, col) = Array2DUtil.getRowCol(array2d)
     result = Image.new(mode, (row, col))
     mapPixel(result, lambda pixel, r, c: array2d[r][c])
     return result
+
 
 def applyKernelToOneChannel(image: ImageType, kernel: list[list[int]]) -> ImageType:
     """
@@ -54,11 +55,12 @@ def applyKernelToOneChannel(image: ImageType, kernel: list[list[int]]) -> ImageT
     filteredIntPixels = Array2DUtil.map(filteredPixels, lambda ele, r, c: int(ele))
     return fromArray2D(filteredIntPixels, image.mode)
 
+
 def applyKernel(image: ImageType, kernel: list[list[int]]) -> ImageType:
     """
     return new image
     """
-    filteredChannelsImages = list(builtins.map(lambda img: applyKernelToOneChannel(img, kernel), image.split()))
+    filteredChannelsImages = list(
+        builtins.map(lambda img: applyKernelToOneChannel(img, kernel), image.split())
+    )
     return Image.merge(image.mode, filteredChannelsImages)
-
-
